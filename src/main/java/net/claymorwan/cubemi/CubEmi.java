a9b12dec7db6a345f7e09f9d82977d4edb8e43cf
claymorwan <claymorw@gmail.com>
2024-08-18 14:14:35 +0000

    Dimensions

--- src/main/java/net/claymorwan/cubemi/CubEmi.java
+++ src/main/java/net/claymorwan/cubemi/CubEmi.java
@@ -6,6 +6,9 @@ import net.claymorwan.cubemi.item.ModItems;
 import net.claymorwan.cubemi.sound.ModSounds;
 import net.fabricmc.api.ModInitializer;
 
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,5 +26,13 @@ public class CubEmi implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModSounds.registerSounds();
 		LOGGER.info("All done :3c");
+
+		// Custom portal
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(ModBlocks.EMI_BLOCK) //Block used to make the portal
+				.lightWithFluid(Fluids.WATER)
+				.destDimID(Identifier.of(CubEmi.MOD_ID, "melodim"))
+				.tintColor(80,133,178)
+				.registerPortal();
 	}
 }
